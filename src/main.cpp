8dba88d65d9be10652fd8132405a34cd2483a154
Aryan <aryanthapliyal@yahoo.co.in>
2025-03-21 19:48:04 +0530

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void invalidCommand(string input){
-  cout<<input<<": command not found"<<std :: endl;
+void invalidCommand(string input) {
+	cout << input << ": command not found" << std ::endl;
+}
+
+void REPL(){
+	while(true){
+		string input;
+		cout << "$ ";
+		getline(std::cin, input);
+		invalidCommand(input);
+	}
 }
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-
-  invalidCommand(input);
+	REPL();
 }
